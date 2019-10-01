7bb669cdfc1d4686f8b98e2e186e3e46b6367a4e
Emmett <postill@ualberta.ca>
2019-09-30 18:59:16 -0600

    fixed everything

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,7 +3,6 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
