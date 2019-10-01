8ee1e6b80ef89c2501417ff071b41c06afc6dfac
raysarinas <raysarinas97@gmail.com>
2019-09-30 19:09:17 -0600

    resolve conflict

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,9 +3,10 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
-    private String colour = "red";
+    private String color = "red";
 
-    public Shape(int x, int y) {
+    public Shape(int x, int y, String color) {
+        this.color = color;
         this.x = x;
         this.y = y;
     }
