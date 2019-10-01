1154dd3081df5fec55dbfcab283a11ca95a87fc2
Jacob Wong <jdw2@ualberta.ca>
2019-09-30 19:07:47 -0600

    🌈🌈🌈🌈🌈🌈

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,7 +3,7 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
-
+    String color = "rainbow";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
