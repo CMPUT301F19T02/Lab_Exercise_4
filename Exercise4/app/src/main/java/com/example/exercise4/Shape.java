ab0a1e4afbd7cd0c39ed50fd86d0e2b5ce472a94
Emmett <postill@ualberta.ca>
2019-09-30 18:41:57 -0600

    fixed color

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,10 +3,9 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
-    String color;
+    String color = "red";
 
-    public Shape(int x, int y, String color) {
-        this.color = color
+    public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
