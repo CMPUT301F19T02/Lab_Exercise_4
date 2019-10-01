fce3f2cdcf1c32dcba75b718245974df8d8804c2
Trishia Gatilogo <gatilogo@ualberta.ca>
2019-09-30 18:45:42 -0600

    Adding color

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,6 +3,7 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "rainbow";
 
     public Shape(int x, int y) {
         this.x = x;
