0415c8d3100de7e1ddaa2a790f05a08551d4ec9a
Emmett <postill@ualberta.ca>
2019-09-30 18:39:10 -0600

    added color

--- Exercise4/app/src/main/java/com/example/exercise4/Shape.java
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -3,8 +3,10 @@ package com.example.exercise4;
 public abstract class Shape {
     private int x;
     private int y;
+    String color;
 
-    public Shape(int x, int y) {
+    public Shape(int x, int y, String color) {
+        this.color = color
         this.x = x;
         this.y = y;
     }
