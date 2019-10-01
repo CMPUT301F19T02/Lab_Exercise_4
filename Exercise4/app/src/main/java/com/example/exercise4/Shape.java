f1622b75a0f25c3fca5250811d988a8e09324560
TheBroMoe <kebbi@ualberta.ca>
2019-09-30 18:24:22 -0600

    add app

---
+++ Exercise4/app/src/main/java/com/example/exercise4/Shape.java
@@ -0,0 +1,11 @@
+package com.example.exercise4;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
